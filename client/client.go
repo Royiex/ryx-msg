73c1cb4ad1a5f6878238bc5c3af92fe61ad2dafc
royex <royexl@proton.me>
2025-12-24 21:14:11 +0100

    basic tui?

--- client/client.go
+++ client/client.go
@@ -7,9 +7,21 @@ import (
 	"fortio.org/terminal/ansipixels"
 )
 
-var loggedin bool =  false
+var loggedin bool = false
+
+func drawLogin(ap *ansipixels.AnsiPixels) {
+	ap.ClearScreen()
+	ap.DrawSquareBox(0, 0, ap.W, ap.H)
+}
+
+func drawBase(ap *ansipixels.AnsiPixels) {
+	ap.ClearScreen()
+	ap.DrawSquareBox(0, 0, ap.W/3, ap.H)
+	ap.DrawSquareBox(ap.W/3, 0, ap.W/3*2, ap.H)
+}
 
 func main() {
+	var input []byte
 	ap := ansipixels.NewAnsiPixels(15)
 	if err := ap.Open(); err != nil {
 		return
@@ -24,26 +36,28 @@ func main() {
 		os.Exit(0)
 	}()
 
-	ap.ClearScreen()
-	ap.WriteAtStr(5, 5, "Hello!")
+	ap.GetSize()
+
+	drawLogin(ap)
 	ap.Out.Flush()
 
 	for {
 		n, err := ap.ReadOrResizeOrSignalOnce()
-		if err != nil {
-			break
-		}
-		if n>0 {
-			input := ap.Data[:n]
+		if err != nil { break }
+		if(n>0) {
+			input = ap.Data[:n]
 			if input[0] == 3  || input[0] == 17 {
 				break
 			}
-
-			ap.ClearScreen()
-			ap.WriteAt(5, 6, "%d", input)
-			ap.Out.Flush()
-
 		}
+
+
+		if(!loggedin) {
+			drawLogin(ap)
+			ap.WriteAt(5, 6, "%d", input)
+		}
+		ap.Out.Flush()
 	}
+
 
 }
